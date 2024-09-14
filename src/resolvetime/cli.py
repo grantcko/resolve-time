96aa2d52f43ce0802e7dfffe4db23d219add5e87
Grant Hall <grantclone@gmail.com>
2024-09-13 19:04:35 -0700

    made typer work by changing relative import of functions module and wrapping all cli code in an app.command() funciton"

--- src/resolvetime/cli.py
+++ src/resolvetime/cli.py
@@ -5,7 +5,7 @@ import subprocess
 import typer
 from datetime import datetime
 import threading
-import .functions
+from . import functions
 import glob
 import os
 
@@ -22,28 +22,33 @@ current_datetime = datetime.now().strftime("%Y%m%d-%H%M%S")
 # get reference to $HOME environmental variable
 home_path = os.environ["HOME"]
 
-#### GET LOG FILES, PROCESS, PRINT SUMMARY ####
+@app.command()
+def run_all():
+    #### GET LOG FILES, PROCESS, PRINT SUMMARY ####
 
-# TODO: create an option for auto or manual log file generation then define the filepath that gets passed to process the logs accordingly
+    # TODO: create an option for auto or manual log file generation then define the filepath that gets passed to process the logs accordingly
 
-# auto generate the log files
-app.command()(functions.auto_gen_logs(current_datetime, home_path))
+    # auto generate the log files
+    app.command()(functions.auto_gen_logs(current_datetime, home_path))
 
-auto_zip_filepath = f"{home_path}/Desktop/DaVinci-Resolve-logs-{current_datetime[:11]}*.tgz"
+    auto_zip_filepath = f"{home_path}/Desktop/DaVinci-Resolve-logs-{current_datetime[:11]}*.tgz"
 
-processed_info = app.command()(functions.process_logs(home_path, current_datetime, txt_filepath, auto_zip_filepath))
+    processed_info = app.command()(functions.process_logs(home_path, current_datetime, txt_filepath, auto_zip_filepath))
 
-summary = app.command()(functions.build_summary(processed_info["info"], processed_info["monthly_info"]))
+    summary = app.command()(functions.build_summary(processed_info["info"], processed_info["monthly_info"]))
 
-print(summary)
+    print(summary)
 
-#### REMOVE LOG FILES ####
+    #### REMOVE LOG FILES ####
 
-# Wait 3 seconds. Not using sleep() because of iteraction with datetime
-threading.Event().wait(3)
+    # Wait 3 seconds. Not using sleep() because of iteraction with datetime
+    threading.Event().wait(3)
 
-# Remove the generated zip file
-subprocess.run(['rm', '-rf', processed_info["zip_filepath"]])
+    # Remove the generated zip file
+    subprocess.run(['rm', '-rf', processed_info["zip_filepath"]])
 
-# Remove the unzipped log folder
-subprocess.run(['rm', '-rf', f"{home_path}/Desktop/Library"])
+    # Remove the unzipped log folder
+    subprocess.run(['rm', '-rf', f"{home_path}/Desktop/Library"])
+
+if __name__ == "__main__":
+    app()

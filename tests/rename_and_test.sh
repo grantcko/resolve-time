bf39fa8fa672b8b9169e0e91d54124dccb049607
Grant Hall <grantclone@gmail.com>
2024-09-17 20:39:16 -0700

    add bash script for changing the existing masterlog files and running the tests

--- tests/rename_and_test.sh
+++ tests/rename_and_test.sh
@@ -1,7 +1,13 @@
 #!/bin/zsh
 
-# Rename masterlog_blank.txt to masterlog.txt
+print "STARTTESTS"
 mv tests/masterlog_blank.txt tests/masterlog.txt
-
-# Run pytest
 pytest
+mv tests/masterlog.txt tests/masterlog_blank.txt
+mv tests/masterlog_missing.txt tests/masterlog.txt
+pytest
+mv tests/masterlog.txt tests/masterlog_missing.txt
+mv tests/masterlog_overlap.txt tests/masterlog.txt
+pytest
+mv tests/masterlog.txt tests/masterlog_overlap.txt
+print "ENDTESTS"
